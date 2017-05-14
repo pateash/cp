f652ece59cc3f06d3b37b7a9818c6a317dc90a6d
Ashish patel <ashishpatel0720@gmail.com>
2017-05-14 16:51:43 +0530

    java project added

---
+++ src/codejava/CodeJava.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codejava;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import sun.util.calendar.JulianCalendar;
+import java.math.BigInteger;
+/**
+ *
+ * @author ashish
+ */
+import java.util.ArrayList;
+public class CodeJava {
+
+   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Solution().solve();  
+    }
+
+}
+class Solution {
+        void solve(){
+
+        }
+}
