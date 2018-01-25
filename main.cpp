0617cfda084a6c0d6bf9c498cc8af26980cd51e7
Ashish Patel <ashishpatel0720@gmail.com>
2018-01-25 19:52:32 +0530

    GCD_ADDED: utility function

--- main.cpp
+++ main.cpp
@@ -142,6 +142,11 @@ ll power(int i,int c){
 template<typename T> T mod(T x){ //this function return |x| value
     return x<0? -x:x;
 }
+template<typename T> T gcd(T a,T b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
 void solve(){
 }
 
