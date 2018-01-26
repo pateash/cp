6e9f2119bb3ab94ecbdaaa6cc13e7efccf51edce
Ashish Patel <ashishpatel0720@gmail.com>
2018-01-26 19:18:01 +0530

    SIEVE,PRIME_CHECK: added

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #define ll long long int
 //container
 #define vi vector<int>
+#define vl vector<ll>
 #define ii pair<int,int>
 #define vii vector<ii>
 //IO
@@ -87,10 +88,9 @@ void test_working(){
 
 //utility classes
 class UnionFind{
+public:
     vi par,rank,setSize;
     int numsets;
-
-public:
     UnionFind(int n){
         par.assign(n,0);rank.assign(n,0);
         setSize.assign(n,1);
@@ -120,12 +120,51 @@ public:
             numsets--;
         }
     }
-    int numDisjointSets(){
-        return numsets;
+};
+
+#define SIEVE_LIMIT int(1e7+10) //must be some greater than 'upperbound'
+class Sieve{
+public:
+    ll _sieve_size; // size for bitset[0....upperbound] for storing 0/1
+    bitset<SIEVE_LIMIT>bs; //10^7 should be enough for most cases _sieve_size<10^7
+    vl primes;//storing all primes from [2..upperbound]
+
+    Sieve(ll upperbound){
+        //building list of primes from [2..upperbound]
+        _sieve_size=upperbound+1; //from 0 to upperbound
+        bs.set(); //set all to prime
+        bs[0]=bs[1]=0; //0 and 1 are not prime
+
+        for(ll i=2;i<=_sieve_size;i++){
+            if(bs[i]){
+                //if it is an prime then cut all which are large than it and have it as factor
+                primes.push_back(i);
+                for(ll j=i*i;j<=_sieve_size;j+=i) // all less than i*i would have been already cutted by others
+                    bs[j]=0;
+            }
+        }
     }
-    int sizeOfSet(int i){
-        return setSize[i];
+
+    void printPrimes(){
+        //print Primes from 2 to upperbound
+        for(auto p:primes)
+            cout<<p<<"\n";
+        cout<<endl;
     }
+
+    bool isPrimeSieve(ll N){
+        if(N<=_sieve_size) return bs[N];
+
+        //if it is not inside the upperbound
+        // we can use stored primes to find if it is prime or not
+        // only can check if N<=(largest Prime)^2
+
+        for(auto p:primes){
+            if(N%p==0) return false;
+        }
+        return true;
+    }
+
 };
 
 //utility functions
@@ -166,6 +205,19 @@ void extendedEuclid(int a,int b) {
     x=x1;
     y=y1;
 }
+bool isPrimeSimple(int n){
+    /*
+     * return if a number is prime or not
+     * O(sqrt(n))
+     */
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int modIFerm(int A,int M){
     /*applying fermat's theorem A^-1=A^(M-2)%M
      * applicable only when M is prime
@@ -184,12 +236,14 @@ int modIEuclid(int A,int M){
 void solve(){
 
 }
+
 int main() {
-    string curdir="/home/ashish/Documents/code/";
-    if(
-            freopen(string(curdir+"in.txt").c_str(),"r",stdin)
+
+    string curdir = "/home/ashish/Documents/code/";
+    if (
+            freopen(string(curdir + "in.txt").c_str(), "r", stdin)
             &&
-            freopen(string(curdir+"out.txt").c_str(), "w", stdout)
+            freopen(string(curdir + "out.txt").c_str(), "w", stdout)
             );
     else {
         cout << "ERROR: " << strerror(errno) << endl;
