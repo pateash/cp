2a09fb58ecd266fe0a4b6b2a1a147d8f12ff3ec1
Ashish Patel <ashishpatel0720@gmail.com>
2018-11-27 09:51:06 +0530

    IMPORTS: added manually as bits/stdc++.h doesn't work apart from linux/unix

--- lib/OtherFunctions.h
+++ lib/OtherFunctions.h
@@ -4,7 +4,53 @@
 #ifndef SAMPLE_OTHERFUNCTIONS_H
 #define SAMPLE_OTHERFUNCTIONS_H
 
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include <cmath>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<stack>
+#include<list>
+#include<unordered_map>
+#include<unordered_set>
+//types
+#define llu long long unsigned int
+#define ll long long int
+//container
+#define vi vector<int>
+#define vl vector<ll>
+#define ii pair<int,int>
+#define vii vector<ii>
+//IO
+#define pr(n)  printf("%d",n)
+#define prl(n) printf("%lld",n)
+#define prf(n) printf("%f",n)
+#define nl printf("\n")
+#define sp printf(" ")
+#define sc(n)  scanf("%d",&n)
+#define scl(n) scanf("%lld",&n)
+#define scf(n) scanf("%f",&n)
+#define scd(n) scanf("%lf",&n)
+//function and loops
+#define mp make_pair
+#define repab(a,b) for(int i=(a);i<(b);i++)
+#define rep(n) for(int i=0;i<n;i++)
+#define repi(i,n) for(int (i)=0;(i)<n;(i)++)
+#define repit(c) for(auto it=(c).begin();it!=(c).end();it++)
+//reading all once
+#define read(n) int n;sc(n)
+#define readl(n) ll n;scl(n)
+#define readf(n) float n;scf(n)
+#define readd(n) double n;scd(n)
+#define call(c) (c).begin(),(c).end()
+//debug
+#define show(a) std::cout<<#a<<" : "<<a<<std::endl;
+using namespace std;
 using namespace std;
 
 //utility functions
