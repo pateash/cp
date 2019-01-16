15fe1b6c8f010b038e5c613d8abc9b757dc2f778
Ashish Patel <ashishpatel0720@gmail.com>
2019-01-17 02:00:38 +0530

    ADDED:  CommonLibs added and used

---
+++ lib/CommonLibs.h
@@ -0,0 +1,108 @@
+//
+// Created by ashish on 27/1/18.
+//
+
+#ifndef SAMPLE_COMMON_LIBS_H
+#define SAMPLE_COMMON_LIBS_H
+
+#include<iostream>
+#include<cstdio>
+#include <cmath>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<stack>
+#include<list>
+#include<unordered_map>
+#include<unordered_set>
+//types
+#define llu long long unsigned int
+#define ll long long int
+#define llu long long unsigned int
+//container
+#define vi vector<int>
+#define vl vector<ll>
+#define ii pair<int,int>
+#define vii vector<ii>
+//IO
+#define pr(n)  printf("%d",n)
+#define prl(n) printf("%lld",n)
+#define prf(n) printf("%f",n)
+#define nl printf("\n")
+#define sp printf(" ")
+#define sc(n)  scanf("%d",&n)
+#define scll(n) scanf("%lld",&n)
+#define scllu(n) scanf("%llu",&n)
+#define scf(n) scanf("%f",&n)
+#define scd(n) scanf("%lf",&n)
+//function and loops
+#define mp make_pair
+#define repab(a,b) for(int i=(a);i<(b);i++)
+#define rep(n) for(int i=0;i<n;i++)
+#define repi(i,n) for(int (i)=0;(i)<n;(i)++)
+#define repit(c) for(auto it=(c).begin();it!=(c).end();it++)
+//reading all once
+#define read(n) int n;sc(n)
+#define readll(n) ll n;scll(n)
+#define readllu(n) llu n;scllu(n)
+#define readf(n) float n;scf(n)
+#define readd(n) double n;scd(n)
+#define call(c) (c).begin(),(c).end()
+//debug
+#define show(a) std::cout<<#a<<" : "<<a<<std::endl;
+using namespace std;
+
+//testing functions
+template <typename T> void show_2d(T container){
+    for(auto it=container.begin();it!=container.end();it++){
+        for(auto it2=it->begin();it2!=it->end();it2++)
+            cout<<*it2<<" ";
+        cout<<endl;
+    }
+    cout<<endl;
+}
+template <typename T> void show_pair1d(T container){
+    for(auto it=container.begin();it!=container.end();it++){
+        cout<<it->first<<" --> "<<it->second;
+        cout<<endl;
+    }
+    cout<<endl;
+}
+template <typename T,typename R> void show_pair(pair<T,R> p){
+    cout<<p.first<<"-> "<<p.second<<endl;
+}
+template <typename T> void show_1d(T &container){
+    cout<<"data in container is: "<<endl;
+    for(auto it=container.begin();it!=container.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+template <typename  T> void show_arr(T arr[],int size){
+    cout<<"content in array is here"<<endl;
+    for (int i = 0; i < size; ++i)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+}
+template<typename T> void show_graph(T AdjList){
+    cout<<"-----------"<<endl;
+    cout<<"showing graph"<<endl;
+    int i=0;
+    for(auto v: AdjList){
+        cout<<i++;
+        for(auto e:v){
+            cout<<"->"<<"["<<e.first<<","<<e.second<<"]";
+        }
+        cout<<endl;
+    }
+    cout<<"-----------"<<endl;
+}
+//debug
+#define show(a) std::cout<<#a<<" : "<<a<<std::endl;
+using namespace std;
+
+#endif
--- lib/Sorting.h
+++ lib/Sorting.h
@@ -8,53 +8,8 @@
 //
 // Created by ashish on 26/1/18.
 //
-#include<iostream>
-#include<cstdio>
-#include <cmath>
-#include<cstring>
-#include<climits>
-#include<algorithm>
-#include<vector>
-#include<list>
-#include<map>
-#include<set>
-#include<stack>
-#include<list>
-#include<unordered_map>
-#include<unordered_set>
-//types
-#define llu long long unsigned int
-#define ll long long int
-//container
-#define vi vector<int>
-#define vl vector<ll>
-#define ii pair<int,int>
-#define vii vector<ii>
-//IO
-#define pr(n)  printf("%d",n)
-#define prl(n) printf("%lld",n)
-#define prf(n) printf("%f",n)
-#define nl printf("\n")
-#define sp printf(" ")
-#define sc(n)  scanf("%d",&n)
-#define scl(n) scanf("%lld",&n)
-#define scf(n) scanf("%f",&n)
-#define scd(n) scanf("%lf",&n)
-//function and loops
-#define mp make_pair
-#define repab(a,b) for(int i=(a);i<(b);i++)
-#define rep(n) for(int i=0;i<n;i++)
-#define repi(i,n) for(int (i)=0;(i)<n;(i)++)
-#define repit(c) for(auto it=(c).begin();it!=(c).end();it++)
-//reading all once
-#define read(n) int n;sc(n)
-#define readl(n) ll n;scl(n)
-#define readf(n) float n;scf(n)
-#define readd(n) double n;scd(n)
-#define call(c) (c).begin(),(c).end()
-//debug
-#define show(a) std::cout<<#a<<" : "<<a<<std::endl;
-using namespace std;
+
+#include "CommonLibs.h"
 
 template<typename T>
 int quick_sort_partition(vector<T> &v,int l,int r){
