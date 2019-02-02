1ad67573528fa5db624e5200162c70f488b07f09
Ashish Patel <ashishpatel0720@gmail.com>
2019-02-02 17:17:35 +0530

    Graph.h: added

---
+++ libs/Graph.h
@@ -0,0 +1,25 @@
+//
+// Created by ashish on 26/1/18.
+//
+#ifndef SAMPLE_GRAPH_H
+#define SAMPLE_GRAPH_H
+
+#include "CommonLibs.h"
+
+//using adjacency list
+// Space O(V+E)
+
+/* HEIGHT, LEVEL and DEPTH of tree
+ * reference link - http://typeocaml.com/2014/11/26/height-depth-and-level-of-a-tree/
+ *
+ * Height(x) - largest path distance from leaf to that node x
+ *
+ * Depth (x) - largest path distance from root to that node x
+ *
+ * Level(x) - Depth(x) + 1, depth of root is 0 , level =1
+ *
+ * */
+
+
+#endif
+
