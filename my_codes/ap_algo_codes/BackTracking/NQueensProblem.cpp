382e55b5b98972dfd3ec7eb131c7c7e775a58916
Ashish Patel <ashishpatel0720@gmail.com>
2019-02-03 22:45:33 +0530

    NqueensProblem

--- my_codes/ap_algo_codes/BackTracking/NQueensProblem.cpp
+++ my_codes/ap_algo_codes/BackTracking/NQueensProblem.cpp
@@ -31,10 +31,11 @@ bool isSafe(int x,int y){
 // we are checking if placing queen at this index is safe,
 // per chess_ans matrix till now
 //As we are going from up to down and left to right we,
-// do not need to check  for letter indices
-// SAME COLUMN CHECK
+// do not need to check  for later indices
+
+// SAME COLUMN CHECK(same row not possible as we are only placing 1 in one row
   for(int i=0;i<x;i++){//same row will not ever have going one by one
-    if(chess_ans[i][y]==1)
+    if(chess_ans[i][y]==1 )
         return false;
   }
 // DIAGONAL CHECK
@@ -52,7 +53,7 @@ bool isSafe(int x,int y){
   return true;
 
 }
-bool solveNQUeens(int xindex) {
+bool solveNQueens(int xindex) {
     extern int size;
     if (xindex == size)//if all indexes has been done
         return true;
@@ -61,7 +62,7 @@ bool solveNQUeens(int xindex) {
     for (int yindex = 0; yindex < size; yindex++) {
         if(isSafe(xindex,yindex)){
             chess_ans[xindex][yindex] = 1;
-            if (solveNQUeens(xindex + 1))
+            if (solveNQueens(xindex + 1))
                 return true;
             else
                 chess_ans[xindex][yindex] = 0;
@@ -75,12 +76,39 @@ int main()
     extern int size;
     cout << "enter the size of chess" << endl;
     cin >> size;
-    init2D(chess_ans);
+
+    init2D(chess_ans); // initialize with all zeroes
     //cout<<"after initialisation\n";
     //show2d(chess_ans);
-    if (solveNQUeens(0))
+    if (solveNQueens(0))
         show2d(chess_ans);
     else
         cout << "No way found!!!" << endl;
     return 0;
 }
+
+
+/*output
+for size = 4
+ your 2d vector is
+0 1 0 0
+0 0 0 1
+1 0 0 0
+0 0 1 0
+
+
+ size =8
+
+ enter the size of chess
+8
+your 2d vector is
+1 0 0 0 0 0 0 0
+0 0 0 0 1 0 0 0
+0 0 0 0 0 0 0 1
+0 0 0 0 0 1 0 0
+0 0 1 0 0 0 0 0
+0 0 0 0 0 0 1 0
+0 1 0 0 0 0 0 0
+0 0 0 1 0 0 0 0
+
+ */
