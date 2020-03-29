b9ee6281f0ea042c97fccbe3413a9b17a72554f2
ashish <ashishpatel0720@gmail.com>
2020-03-29 20:14:30 +0530

    adding

---
+++ src/run.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+#include <cmath>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<stack>
+#include<list>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+
+#include "libs/"
+using namespace std;
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
