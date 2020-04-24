1cd246dec5c4d71245f161ffc8ea485e629ba8c7
ashish <ashishpatel0720@gmail.com>
2020-04-25 01:59:50 +0530

    [IO]: new method added in binary utils

--- src/main/libs/BinarySearchUtils.h
+++ src/main/libs/BinarySearchUtils.h
@@ -5,27 +5,27 @@
 
 #define INVALID -1
 
-class BinarySearchUtils{
+//tutorial - https://www.youtube.com/watch?v=GU7DpgHINWQ
+class BinarySearchUtils {
 
     template<typename T>
-    static int binarySearchSmallerOrEqualUtility(int start, int end,vector<T>& v, T value) {
+    static int binarySearchSmallerOrEqualUtility(int start, int end, vector<T> &v, T value) {
 
         //base case same as all binary search problems
-        if(start>end)
+        if (start > end)
             return INVALID;
 
-        int mid=(start+end)/2;
+        int mid = (start + end) / 2;
 
         int ans;
         //if value is equal
-        if(v[mid]==value)
-            ans= mid;
-        else if(value < v[mid]){ // value is smaller go to left
-            ans = binarySearchSmallerOrEqualUtility(start,mid-1,v,value);
-        }
-        else{//value is larger go to right
+        if (v[mid] == value)
+            ans = mid;
+        else if (value < v[mid]) { // value is smaller go to left
+            ans = binarySearchSmallerOrEqualUtility(start, mid - 1, v, value);
+        } else {//value is larger go to right
 
-            int index=binarySearchSmallerOrEqualUtility(mid+1,end,v,value);
+            int index = binarySearchSmallerOrEqualUtility(mid + 1, end, v, value);
             /*
              as we are finding equal or less here is a catch, it is possible that we don't get any value while going to right
              we might get nothing ( INVALID),
@@ -33,38 +33,37 @@ class BinarySearchUtils{
              Reason - we have to find equal or less than value, which is already greater than v[mid], so if we do not find any value there,
              this is ofcourse the answer.
             */
-            if(index==INVALID) //could not able to find solution at the right side
+            if (index == INVALID) //could not able to find solution at the right side
                 ans = mid;
             else
-                ans= index; // if solution is found
+                ans = index; // if solution is found
         }
         return ans;
     }
 
     template<typename T>
-    static int binarySearchGreaterOrEqualUtility(int start, int end,vector<T>& v, T value) {
+    static int binarySearchGreaterOrEqualUtility(int start, int end, vector<T> &v, T value) {
 
         //base case same as all binary search problems
-        if(start>end)
+        if (start > end)
             return INVALID;
 
-        int mid=(start+end)/2;
+        int mid = (start + end) / 2;
 
         int ans;
         //if value is equal
-        if(v[mid]==value)
-            ans= mid;
-        else if(value > v[mid]){ // value is greater simply go to right ( at least greater than value required )
-            ans = binarySearchGreaterOrEqualUtility(mid+1,end,v,value);
-        }
-        else{//value is smaller go to left with check for invalid result
+        if (v[mid] == value)
+            ans = mid;
+        else if (value > v[mid]) { // value is greater simply go to right ( at least greater than value required )
+            ans = binarySearchGreaterOrEqualUtility(mid + 1, end, v, value);
+        } else {//value is smaller go to left with check for invalid result
 
-            int index=binarySearchGreaterOrEqualUtility(start,mid-1,v,value);
+            int index = binarySearchGreaterOrEqualUtility(start, mid - 1, v, value);
 
-            if(index==INVALID) //could not able to find solution at the right side
+            if (index == INVALID) //could not able to find solution at the right side
                 ans = mid;
             else
-                ans= index; // if solution is found
+                ans = index; // if solution is found
         }
         return ans;
     }
@@ -73,27 +72,27 @@ public:
 
     /*this method returns an index of element equal(if exists in array) or just greater in sorted array in O(log n)*/
     template<typename T>
-    static int binarySearchSmallerOrEqual(vector<T>& v, T value){
-        return binarySearchSmallerOrEqualUtility(0,v.size()-1,v,value);
+    static int binarySearchSmallerOrEqual(vector<T> &v, T value) {
+        return binarySearchSmallerOrEqualUtility(0, v.size() - 1, v, value);
     }
 
     /*this method returns an index of element equal(if exists in array) or just greater in sorted array in O(log n)*/
     template<typename T>
-    static int binarySearchGreaterOrEqual(vector<T>& v, T value){
-        return binarySearchGreaterOrEqualUtility(0,v.size()-1,v,value);
+    static int binarySearchGreaterOrEqual(vector<T> &v, T value) {
+        return binarySearchGreaterOrEqualUtility(0, v.size() - 1, v, value);
     }
 
     template<typename T>
-    static T binarySearchSmallerOrEqualv2(vector<T>& v, T value){
-        T ans=INVALID;// keep INT_MAX for int and LONG_MAX for long
-        int low=0,high=v.size()-1;
+    static T binarySearchSmallerOrEqualv2(vector<T> &v, T value) {
+        T ans = INVALID;// keep INT_MAX for int and LONG_MAX for long
+        int low = 0, high = v.size() - 1;
 
-        while(low<=high){
-            int mid=low + (high-low)/2; // works and avoids overflow incase low+high > INT_MAX
-            if(v[mid] <=value){ // first v[mid] >=value
-                ans=v[mid];
-                low=mid+1;
-            }else{
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // works and avoids overflow incase low+high > INT_MAX
+            if (v[mid] <= value) { // first v[mid] >=value
+                ans = v[mid];
+                low = mid + 1;
+            } else {
                 high = mid - 1; // go and check if we able to find anything smaller
             }
         }
@@ -103,26 +102,63 @@ public:
     // this  is also called lowerbound
     // works and avoids overflow incase low+high > INT_MAX
     template<typename T>
-    static T binarySearchGreaterOrEqualv2(vector<T>& v, T value){
-        T ans=INVALID;
-        int low=0,high=v.size()-1;
+    static T binarySearchGreaterOrEqualv2(vector<T> &v, T value) {
+        T ans = INVALID;
+        int low = 0, high = v.size() - 1;
 
-        while(low<=high){
-            int mid=low + (high-low)/2;
-            if(v[mid] >=value){ // first v[mid] >=value
-                ans=v[mid]; // update
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (v[mid] >= value) { // first v[mid] >=value
+                ans = v[mid]; // update
                 high = mid - 1; // go and check if we able to find anything smaller
-            }else{
-                low=mid+1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
     }
 
+
+    // smallest element in sorted and rotated array
+    // assumption there are not duplicates
+
+    template<typename T>
+    static T binarySearchSmallestElementInRotatedArray(vector<T> v) {
+
+        int n = v.size();
+
+        if(n==1)
+            return v[0];
+
+        if(v[0] < v[n-1])
+            return v.front(); // no rotation is done ( first < last )
+
+        int low=0,high=n-1;
+
+/*
+ * We need to find split, ie. where the rotation is, then we can find next element as ans.
+ * ex. { 4, 5, 6, 7 , 0, 1,2},
+ * rotation is done ^^^, so answer is 0.
+ */
+        int ans;
+        int first = v[0];
+        while(low<=high){
+            // Find the mid element
+            int mid=low+(high-low)/2;
+
+            // are we in the bigger part or the smaller part
+            if(v[mid] >= first){ //bigger part, go to the right
+                low = mid+1;
+            } else { // go to the left part, answer could be this or min then this one.
+                ans = v[mid];
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 };
 
-void testBinarySearchUtils()
-{
+void testBinarySearchUtils() {
    vi v={10,20,30,40,50};
 
    // --------- equal of smaller
@@ -187,9 +223,18 @@ void testBinarySearchUtils()
     assert(10== BinarySearchUtils::binarySearchGreaterOrEqualv2<int>(v, 9));
 
 
+    // Binary searching smallest element in sorted and rotated array
+    assert(0 == BinarySearchUtils::binarySearchSmallestElementInRotatedArray(
+            vector<int>({ 4, 5, 6,7 , 9, 0,1})));
+    assert(1==BinarySearchUtils::binarySearchSmallestElementInRotatedArray(
+            vector<int>({ 4, 5, 6,7 , 9, 1,2})));
+    assert(0==BinarySearchUtils::binarySearchSmallestElementInRotatedArray(
+            vector<int>({ 4, 5, 6,7 , 9, 0,2})));
 
-
+    assert(1==BinarySearchUtils::binarySearchSmallestElementInRotatedArray(
+            vector<int>({2, 1})));
 
 
 }
+
 #endif
