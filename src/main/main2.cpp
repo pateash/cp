67519dc9bfa8386721e98fb1b39f22bbee6c18d3
ashish <ashishpatel0720@gmail.com>
2020-11-05 08:50:01 +0530

    [IO]: barebone main.cpp added

---
+++ src/main/main2.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+#include<cstdio>
+#include <cmath>
+#include<cstring>
+#include <cassert>
+#include<climits>
+#include<algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<stack>
+#include<list>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+
+#define MOD int(1e9+7)
+#define INF int(1e9+1)
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
