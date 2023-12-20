fdc983bf35eba236547cbac068ece66d1bd8e11f
Ashish Patel <ashishpatel0720@gmail.com>
2023-12-21 03:50:41 +0530

    Refactor StringUtils class and update tests
    StringUtils class has been refactored to include encoding and decoding methods that use a new ENCODING_DELIMITER constant. The encode and decode methods have been adapted to work with this delimiter for a more efficient string handling. Corresponding tests have been updated to reflect these changes.

--- src/main/libs/StringUtils.h
+++ src/main/libs/StringUtils.h
@@ -3,10 +3,10 @@
 
 #include "CommonLibs.h"
 
-
 /*works for vectors etc. also just change type*/
 class StringUtils{
-
+public:
+    static const char ENCODING_DELIMITER = '#';
     static void generatePermutationUtils(int i, string &s){
         /* if i has come to last of string*/
         if(i==s.size()-1) {
@@ -64,24 +64,50 @@ public:
          * https://onedrive.live.com/view.aspx?resid=9B1BD26B02F2442D%21153&id=documents&wd=target%28LeetCode%20Problems%2FArray%20and%20Hashing.one%7C5F798AC7-7179-E64C-84A2-FB1CD88468EC%2FEncode%20and%20Decode%20Strings%20-%20H%7C01B8C7E8-6458-5944-A3A5-B1E1939AFA1C%2F%29
 onenote:https://d.docs.live.net/9b1bd26b02f2442d/Documents/notes/Competetive%20Programming%20Notes/LeetCode%20Problems/Array%20and%20Hashing.one#Encode%20and%20Decode%20Strings%20-%20H&section-id={5F798AC7-7179-E64C-84A2-FB1CD88468EC}&page-id={01B8C7E8-6458-5944-A3A5-B1E1939AFA1C}&end
         */
-        static string encode(vector<string> &s){
-
+        static string encode(vector<string> &inputStrs){
+            string encodedStr = "";
+            for(auto s: inputStrs){
+                encodedStr+= to_string(s.size())+ENCODING_DELIMITER+s;
+            }
+            return encodedStr;
         }
 
-        static vector<string> decode(string &s){
-
+        static vector<string> decode(string &encodedStr){
+            vector<string> decodedStrs;
+            int index=0; // will always be on the starting or length
+            while(index<encodedStr.size())
+            {
+                // search for the first occurence of delimiter after i index in string and return index of first find
+                // string::npos if no occurence found
+               auto delIndex =  encodedStr.find(ENCODING_DELIMITER,index);
+               // length will be between i and pos
+               auto strLength = stoi(encodedStr.substr(index,delIndex-index+1)); // pos, length need to be passed
+               //actual string is starting from delIndex+1, and with strLength
+               auto actualStr = encodedStr.substr(delIndex+1, strLength);
+               decodedStrs.push_back(actualStr);
+               // now index will move to new length
+               index = delIndex+strLength+1;
+            }
+           return decodedStrs;
         }
 };
 
 void testStringUtils()
 {
-//    assert(false);
-//    assert(
-//            ArrayUtils<int>::findSumOfAbsoluteDifferenceOfAllPairs({1,2,3,4,5})
-//            == 20);
-//    assert(
-//            ArrayUtils<int>::findSumOfAbsoluteDifferenceOfAllPairs({2,3,5,7})
-//            == 17);
+    vector<string> str = {"lint","code","love","you"};
+    string encodedStr = StringUtils::encode(str);
+    assert(encodedStr=="4#lint4#code4#love3#you");
+    vector<string> decodedStrs = StringUtils::decode(encodedStr);
+    for(int i=0;i<str.size();i++)
+        assert(str[i]==decodedStrs[i]);
+
+    str = {"we", "say", ":", "yes"};
+    encodedStr = StringUtils::encode(str);
+    assert(encodedStr=="2#we3#say1#:3#yes");
+    decodedStrs = StringUtils::decode(encodedStr);
+    for(int i=0;i<str.size();i++)
+        assert(str[i]==decodedStrs[i]);
+
     cout<<"------------"<<endl;
 }
 
