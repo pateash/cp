e56f4f38dbd3662b188e2e9993ad298522a4b429
Ashish Patel <ashishpatel0720@gmail.com>
2024-01-22 23:36:52 +0530

    Refactor StringUtils class and update tests
    StringUtils class has been refactored to include encoding and decoding methods that use a new ENCODING_DELIMITER constant. The encode and decode methods have been adapted to work with this delimiter for a more efficient string handling. Corresponding tests have been updated to reflect these changes.

--- src/main/libs/Primes.h
+++ src/main/libs/Primes.h
@@ -15,57 +15,78 @@
 class Sieve{
 private:
     long long _sieve_size; // size for bitset[0....max_number] for storing 0/1
-    bitset<SIEVE_LIMIT>bs; //10^7 should be enough for most cases _sieve_size<10^7
+    bitset<SIEVE_LIMIT> bs; //10^7 should be enough for most cases _sieve_size<10^7
     vector<long long> primes;//storing all primes from [2..max_number]
 
 public:
-    Sieve(long long  max_number){
+    Sieve(long long max_number) {
         //building list of primes from [2..max_number]
-        _sieve_size= max_number + 1; //from 0 to max_number
+        _sieve_size = max_number + 1; //from 0 to max_number
         bs.set(); //set all to prime
-        bs[0]=bs[1]=0; //0 and 1 are not prime
+        bs[0] = bs[1] = 0; //0 and 1 are not prime
 
-        for(long long i=2;i<=_sieve_size;i++){
-            if(bs[i]){
+        for (long long i = 2; i <= _sieve_size; i++) {
+            if (bs[i]) {
                 //if it is an prime then cut all which are large than it and have it as factor
                 primes.push_back(i);
-                for(long long j=i*i;j<=_sieve_size;j+=i) // all less than i*i would have been already cutted by others
-                    bs[j]=0;
+                for (long long j = i * i;
+                     j <= _sieve_size; j += i) // all less than i*i would have been already cutted by others
+                    bs[j] = 0;
             }
         }
     }
 
-    void printPrimes(){
+    void printPrimes() {
         //print Primes from 2 to max_number
-        for(auto p:primes)
-            cout<<p<<"\n";
-        cout<<endl;
+        for (auto p: primes)
+            cout << p << "\n";
+        cout << endl;
     }
 
-    bool isPrimeSieve(long long N){
-        if(N<=_sieve_size) return bs[N];
+    bool isPrimeSieve(long long N) {
+        if (N <= _sieve_size) return bs[N];
 
         //if it is not inside the max_number
         // we can use stored primes to find if it is prime or not
         // only can check if N<=(largest Prime)^2
 
-        for(auto p:primes){
-            if(N%p==0) return false;
+        for (auto p: primes) {
+            if (N % p == 0) return false;
         }
         return true;
     }
 
 };
 
-bool isPrimeSimple(int n){
-    /*
-     * return if a number is prime or not
-     * O(sqrt(n))
-     */
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) {
+/*
+ * return if a number is prime or not
+ * O(sqrt(n))
+ */
+bool isPrimeSimple(int n) {
+	if (n < 2)
+		return false;
+	if (n == 2)
+		return true;
+	if (n % 2 == 0)
+		return false;
+
+	for (int i = 3, sz = sqrt(n); i <= sz; i += 2) {
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+// log(N)
+bool isPrimeFast(int number) {
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+
+    if (number % 2 == 0 || number % 3 == 0) return false;
+
+    for (int i = 5; i * i <= number; i += 6) {
+        if (number % i == 0 || number % (i + 2) == 0)
             return false;
-        }
     }
     return true;
 }
